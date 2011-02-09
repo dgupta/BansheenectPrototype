06b35549cf1883096b3a6ad15372f489e543266a
mrkaiser <dev.gupta.k@gmail.com>
2011-02-08 21:06:18 -0500

    Adding the main files

---
+++ BanshenectPrototype/Main.cs
@@ -0,0 +1,17 @@
+using System;
+//load the OpenNI and the NITE.net libraries
+using xn;
+using xnv;
+
+
+namespace BanshenectPrototype
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+		}
+	}
+}
+
